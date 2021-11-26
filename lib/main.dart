ccddb861f363a75f0930a88947e22ab4319381c3
kidusmik <kidusmik@gmail.com>
2021-11-26 19:16:35 +0300

    Add container

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            width: 100,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: const EdgeInsets.all(20),
+            color: Colors.white,
+            child: const Text('Hello'),
+          ),
+        ),
       ),
     );
   }
